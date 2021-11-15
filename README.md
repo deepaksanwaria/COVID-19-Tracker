2ccf90fe6a073995aff6579c802b72d6659211ac
Deepak Agarwal <61456837+deepaksanwaria@users.noreply.github.com>
2021-11-15 13:59:38 +0530

    Changed the API

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # COVID-19 case tracker (Google Chrome Extension)
-It is a google chrome extension that tracks the COVID cases. Currently, it only tracks the COVID case for Jharkhand, INDIA. It shows you the *Total confirm cases, Active cases, Death Cases, Recovered Cases*.
+It is a google chrome extension that tracks the COVID cases. It shows you the *Total confirm cases, Active cases, Death Cases, Recovered Cases*.
 <br/><br>
 
 # Deployment
@@ -26,7 +26,7 @@ chrome://extensions
 <br/><br/>
 
 # API used
-https://github.com/covid19india/api.git
+https://corona.lmao.ninja
 <br/><br/>
 
 # Screenshot
@@ -34,7 +34,7 @@ https://github.com/covid19india/api.git
 <br/><br/>
 # Author 
 **DEEPAK AGARWAL** <br/><br/>
-![Logo](https://github.com/deepaksanwaria/Swarastra/blob/7377029fa0a064e39c19538f6c22cf89b44c1a03/Readme-Image/Deepak-Agarwal.png?raw=true)
+![IMAGE](Images/Deepak-Agarwal.png)
 <br/><br/>
 [![portfolio](https://img.shields.io/badge/portfolio-000?style=for-the-badge&logo=ko-fi&logoColor=white)](https://github.com/deepaksanwaria/)
 [![linkedin](https://img.shields.io/badge/linkedin-0A66C2?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/deepak-agarwal-2460831a9/)
--- getdata.js
+++ getdata.js
@@ -1,25 +1,47 @@
-fetch('https://data.covid19india.org/v4/min/data.min.json')
+//Fetching the data from the api
+fetch('https://corona.lmao.ninja/v2/countries')
     .then(data => data.json())
     .then(coviddata => {
-        const confirmed = coviddata.JH.total.confirmed;
+        //Fetching the active cases
+        const confirmed = coviddata.find(coviddata => coviddata.country === 'India').cases;
         const confirmedelement = document.getElementById('confirmed');
 
-        const deceased = coviddata.JH.total.deceased;
+        //Fetching the deceased cases
+        const deceased = coviddata.find(coviddata => coviddata.country === 'India').deaths;
         const deathdelement = document.getElementById('death');
 
-        const recovered = coviddata.JH.total.recovered;
+        //Fetching the recovered cases
+        const recovered = coviddata.find(coviddata => coviddata.country === 'India').recovered;
         const dischargedelement = document.getElementById('discharged');
 
-        const active = document.getElementById('active');
+        // Fetching the active cases
+        const active_case = coviddata.find(coviddata => coviddata.country === 'India').active;
+        const activeelement = document.getElementById('active');
 
-        const last_updated = coviddata.JH.meta.last_updated;
+        //Fetching the last updated time
+        const last_updated = coviddata.find(coviddata => coviddata.country === 'India').updated;
         const last_update = document.getElementById('last_update');
 
-        const active_case = confirmed - (deceased + recovered);
-
+        // Setting the values to the elements
         confirmedelement.innerHTML = confirmed;
         deathdelement.innerHTML = deceased;
         dischargedelement.innerHTML = recovered;
-        last_update.innerHTML = last_updated;
-        active.innerHTML = active_case;
+        last_update.innerHTML = msToTime(last_updated);
+        activeelement.innerHTML = active_case;
+
     })
+    .catch(err => console.log(err)); //Catching the error and displaying it in the console.
+
+// function to convert milliseconds to time in hours, minutes 
+function msToTime(duration) {
+    var milliseconds = Math.floor((duration % 1000) / 100),
+        seconds = Math.floor((duration / 1000) % 60),
+        minutes = Math.floor((duration / (1000 * 60)) % 60),
+        hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
+
+    // Adding 0 if the value is less than 10
+    minutes = (minutes < 10) ? "0" + minutes : minutes;
+    seconds = (seconds < 10) ? "0" + seconds : seconds;
+
+    return hours + " hours " + minutes + " minutes ago. "; //Returning the time in hours, minutes
+}
